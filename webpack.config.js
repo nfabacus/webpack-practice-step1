a83200ff6c148a5126a14b5e563f69b76741edef
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-04-29 03:27:49 +0100

    adds babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,14 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename:'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/   //with the rejex, this will specifically look for js file to apply the loader.
+      }
+    ]
   }
 };
 
