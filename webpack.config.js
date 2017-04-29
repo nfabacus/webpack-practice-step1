a8f5f875c8cb4d1f0373fd016d35536871074782
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-04-29 05:22:48 +0100

    uses ExtractTextPlugin and css-loader to combine all the css files into style.css file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   entry: './src/index.js',
@@ -11,9 +12,19 @@ const config = {
       {
         use: 'babel-loader',
         test: /\.js$/   //with the rejex, this will specifically look for js file to apply the loader.
+      },
+      {
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader'
+        }),
+        // use: ['style-loader', 'css-loader'], //these loaders are applied from right to left. so, css-loader, then style-loader is applied.
+        test: /\.css$/
       }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
 };
 
 module.exports = config;
